d2fe46bf1cbaf5488409fc7912b10e6af9ce7413
Станислав Алексеевич Третьяков <SATretiyakov@sberbank.ru>
2021-10-22 08:55:13 +0300

    cglib

--- microservices/miscexample/src/main/java/net/medrag/microservices/misc/bpp/BppProxyBuilder.java
+++ microservices/miscexample/src/main/java/net/medrag/microservices/misc/bpp/BppProxyBuilder.java
@@ -3,6 +3,8 @@ package net.medrag.microservices.misc.bpp;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Proxy;
@@ -44,26 +46,61 @@ public class BppProxyBuilder implements BeanPostProcessor {
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         final ImmutablePair<Object, Set<InterceptionData>> pair = beans.get(beanName);
         if (pair != null) {
-            Object storedBean = pair.left;
-
-            final Object proxiedBean = Proxy.newProxyInstance(
-                    storedBean.getClass().getClassLoader(),
-                    storedBean.getClass().getInterfaces(),
-                    (proxy, method, args) -> {
-                        final var data = pair.right;
-                        final Optional<InterceptionData> first = data.stream().filter(x -> x.methodName().equals(method.getName())).findFirst();
-                        if (first.isPresent()) {
-                            System.out.println(first.get().printBefore);
-                            final Object invoke = method.invoke(bean, args);
-                            System.out.println(first.get().printAfter);
-                            return invoke;
-                        } else {
-                            return method.invoke(bean, args);
-                        }
-                    }
-            );
-            return BeanPostProcessor.super.postProcessAfterInitialization(proxiedBean, beanName);
+            if (Math.random() > 0.5) {
+                return buildProxy(bean, beanName, pair);
+            } else {
+                return buildEnhancer(bean, beanName, pair);
+            }
         }
         return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+
+    /**
+     * More useful CGLIB methods here: https://www.baeldung.com/cglib
+     */
+    private Object buildEnhancer(Object bean, String beanName, ImmutablePair<Object, Set<InterceptionData>> pair) {
+
+        System.out.println("Using CGLIB...");
+        Object storedBean = pair.left;
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(storedBean.getClass());
+        enhancer.setCallback((MethodInterceptor) (obj, method, args, proxy) -> {
+            final var data = pair.right;
+            final Optional<InterceptionData> first = data.stream().filter(x -> x.methodName().equals(method.getName())).findFirst();
+            if (first.isPresent()) {
+                System.out.println(first.get().printBefore);
+                final Object invoke = method.invoke(bean, args);
+                System.out.println(first.get().printAfter);
+                return invoke;
+            } else {
+                return proxy.invokeSuper(obj, args);
+            }
+        });
+        return BeanPostProcessor.super.postProcessAfterInitialization(enhancer.create(), beanName);
+    }
+
+    private Object buildProxy(Object bean, String beanName, ImmutablePair<Object, Set<InterceptionData>> pair) {
+
+        System.out.println("Using proxy...");
+        Object storedBean = pair.left;
+
+        final Object proxiedBean = Proxy.newProxyInstance(
+                storedBean.getClass().getClassLoader(),
+                storedBean.getClass().getInterfaces(),
+                (proxy, method, args) -> {
+                    final var data = pair.right;
+                    final Optional<InterceptionData> first = data.stream().filter(x -> x.methodName().equals(method.getName())).findFirst();
+                    if (first.isPresent()) {
+                        System.out.println(first.get().printBefore);
+                        final Object invoke = method.invoke(bean, args);
+                        System.out.println(first.get().printAfter);
+                        return invoke;
+                    } else {
+                        return method.invoke(bean, args);
+                    }
+                }
+        );
+        return BeanPostProcessor.super.postProcessAfterInitialization(proxiedBean, beanName);
     }
 }
