d7989af50e314b05576e79d3dfb8ff35a9082e2d
Stanislav Tretiakov <s.a.tretiakov@jettycloud.com>
2022-09-04 11:06:02 +0400

    springexamples updates

--- springexamples/src/main/java/net/medrag/springexamples/config/ComplexConfig.java
+++ springexamples/src/main/java/net/medrag/springexamples/config/ComplexConfig.java
@@ -1,6 +1,7 @@
 package net.medrag.springexamples.config;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
@@ -17,10 +18,20 @@ public final class ComplexConfig {
     private final String one;
     private final String two;
     private final String[] array;
+    private final InnerComplexConfig innerComplexConfig;
 
-    public ComplexConfig(String one, String two, String[] array) {
+    public ComplexConfig(String one, String two, String[] array, InnerComplexConfig innerComplexConfig) {
         this.one = one;
         this.two = two;
         this.array = array == null ? new String[0] : array;
+        this.innerComplexConfig = innerComplexConfig;
+    }
+
+    @Getter
+    @ToString
+    @ConstructorBinding
+    @RequiredArgsConstructor
+    public static final class InnerComplexConfig {
+        private final String inner;
     }
 }
