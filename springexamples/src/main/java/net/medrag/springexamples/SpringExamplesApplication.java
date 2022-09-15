c7aff87035a0fc05c1d8e22aa77fc3df43c73f54
Stanislav Tretiakov <s.a.tretiakov@jettycloud.com>
2022-09-15 16:21:38 +0400

    properties precedence

--- springexamples/src/main/java/net/medrag/springexamples/SpringExamplesApplication.java
+++ springexamples/src/main/java/net/medrag/springexamples/SpringExamplesApplication.java
@@ -6,8 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringExamplesApplication {
 
+    // SOME_ARBITRARY_PROPERTY=yyy Precedence for Spring App Context: 2
+    private static final String ENV_VAR = "SOME_ARBITRARY_PROPERTY";
+    // -Dsome.arbitrary.property=xxx Precedence for Spring App Context: 1
+    private static final String PROPERTY = "some.arbitrary.property";
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringExamplesApplication.class, args);
-	}
+
+        System.out.println("----");
+        System.out.println(System.getenv(ENV_VAR));
+//        System.out.println(System.getenv());
+        System.out.println(System.getProperty(PROPERTY));
+//        System.out.println(System.getProperties());
+        System.out.println("----");
+        final var context = SpringApplication.run(SpringExamplesApplication.class, args);
+        final var property = context.getEnvironment().getProperty(PROPERTY);
+        System.out.println("----");
+        System.out.println(property);
+        System.out.println("----");
+    }
 
 }
